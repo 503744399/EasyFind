646b1ae492a36da3c59a97bcc3dfd7550f411c42
503744399 <85235087+503744399@users.noreply.github.com>
2022-11-09 16:25:07 +0800

    Add files via upload

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"regexp"
+)
+
+var filepath string
+
+func main() {
+	flag.StringVar(&filepath, "t", "target", "文件路径，默认为空")
+	flag.Parse()
+	content, _ := ioutil.ReadFile(filepath)
+	reg1 := regexp.MustCompile(`flag.*}`)
+	result1 := reg1.FindAllStringSubmatch(string(content), -1)
+	fmt.Println(result1)
+}
